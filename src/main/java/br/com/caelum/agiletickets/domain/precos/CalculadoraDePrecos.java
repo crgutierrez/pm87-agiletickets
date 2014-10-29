26409ec4c754a64c62fc54ae0dca112c9df2ff2e
Cristiano Gutierrez <cristiano.vdrive@gmail.com>
2014-10-29 07:38:23 -0700

    minor

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -7,8 +7,7 @@ import br.com.caelum.agiletickets.models.Sessao;
 public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
-		BigDecimal preco;
-		preco = sessao.getEspetaculo().getTipo().calculaPreco(sessao);
+		BigDecimal preco = sessao.getEspetaculo().getTipo().calculaPreco(sessao);
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
